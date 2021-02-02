1625a635c7f50c1b4b2bd0026dc79500bbd914b7
nitishapal <palnitisshaece26@gmail.com>
2021-02-02 23:37:59 +0530

    added search functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularSearchLocation';
+  results = [];
+  searchForm: FormGroup;
+  searchitem: any = '';
+  searchedItemList: any = [];
+  constructor(private http: HttpClient, private formBuilder: FormBuilder){
+    this.searchForm = this.formBuilder.group({
+      searchinput: new FormControl('', [Validators.required]),
+    });
+  }
+  search(term: any){
+    console.log(term);
+    if(term !== ''){
+    this.http.get('https://randomuser.me/api/?nat=' + term).toPromise().then((data: any) => {
+      console.log(data);
+      if (data.results) {
+      this.searchitem = ''
+      this.results = data.results;
+      this.searchitem = data.results[0].location.country;
+      let itemObj = {name: term, location:this.searchitem};
+      this.searchedItemList.push(itemObj);
+      }
+    }).catch((error: any) => {
+      console.log(error);
+      alert('No search result found.');
+    })
+  }
+  }
 }
